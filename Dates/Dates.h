574b747cf51b3be0076a3a49e3b235d1f22b3b18
Rob Williams <rob@ontometrics.com>
2015-05-24 15:43:09 -0700

    Initial commit

---
+++ Dates/Dates.h
@@ -0,0 +1,19 @@
+//
+//  Dates.h
+//  Dates
+//
+//  Created by Rob Williams on 5/24/15.
+//  Copyright (c) 2015 ontometrics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dates.
+FOUNDATION_EXPORT double DatesVersionNumber;
+
+//! Project version string for Dates.
+FOUNDATION_EXPORT const unsigned char DatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dates/PublicHeader.h>
+
+
