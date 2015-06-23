3fd4b6c0c1c77bbd2068b819438c558b01edd84f
Noura Hassan <noura.hassan@ontometrics.com>
2015-06-23 15:25:21 +0200

    make SystemClock struct

--- Dates/SystemClock.swift
+++ Dates/SystemClock.swift
@@ -6,16 +6,15 @@
 //  Copyright (c) 2015 Ontometrics. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class SystemClock: NSObject {
+struct SystemClock {
     
     static var shift:Double = 0
     static var useSpecificDate = false //turn this to true to use the date in specificDay property
     static var specificDay:Dictionary<String, Int> = ["day": 2, "month": 12, "year": 2015, "hour": 12, "minute": 30]
     
-    @objc
-    class func now() -> NSDate {
+    static func now() -> NSDate {
         #if (arch(i386) || arch(x86_64)) && os(iOS)
             
             if let date = dateForSimulationTime() where useSpecificDate == false {
@@ -34,7 +33,7 @@ class SystemClock: NSObject {
         #endif
     }
     
-    private class func dateForSpecificDay() -> NSDate {
+    private static func dateForSpecificDay() -> NSDate {
         let parts:NSDateComponents = NSDateComponents()
         parts.day = specificDay["day"]!
         parts.month = specificDay["month"]!
@@ -50,7 +49,7 @@ class SystemClock: NSObject {
     /**
     * Date by adding timeInterval from run arguments
     */
-    private class func dateForSimulationTime() -> NSDate? {
+    private static func dateForSimulationTime() -> NSDate? {
         let args = NSArray(array:NSProcessInfo.processInfo().arguments)
         let filteredArgs = args.filteredArrayUsingPredicate(NSPredicate(format: "SELF contains[cd] %@", "simulationTimeInterval"))
         if(filteredArgs.count > 0){
