1a9690c9457e581a53c13c60ccc8cdb2cdba85c6
Noura Hassan <noura.hassan@ontometrics.com>
2015-10-14 23:56:22 +0200

    create a target for watchOS

---
+++ Dates (watchOS)/Dates (watchOS).h
@@ -0,0 +1,19 @@
+//
+//  Dates (watchOS).h
+//  Dates (watchOS)
+//
+//  Created by Noura on 10/14/15.
+//  Copyright © 2015 ontometrics. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Dates (watchOS).
+FOUNDATION_EXPORT double Dates__watchOS_VersionNumber;
+
+//! Project version string for Dates (watchOS).
+FOUNDATION_EXPORT const unsigned char Dates__watchOS_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dates__watchOS_/PublicHeader.h>
+
+
