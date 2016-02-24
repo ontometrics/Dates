20ec38964fc44cd807cad929366ae22d84245fa2
Noura Hassan <noura.hassan@ontometrics.com>
2016-02-24 20:16:04 +0200

    add target for OS X

---
+++ DatesOSX/DatesOSX.h
@@ -0,0 +1,19 @@
+//
+//  DatesOSX.h
+//  DatesOSX
+//
+//  Created by Noura on 2/24/16.
+//  Copyright © 2016 ontometrics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DatesOSX.
+FOUNDATION_EXPORT double DatesOSXVersionNumber;
+
+//! Project version string for DatesOSX.
+FOUNDATION_EXPORT const unsigned char DatesOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DatesOSX/PublicHeader.h>
+
+
